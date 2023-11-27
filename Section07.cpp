7cb1ea3d3ec2b7e4149d69b12ff3b0f02ba0571f
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-11-27 22:41:02 +0900

    Initial commit

---
+++ Section07.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void Section07()
+{
+	// 問題①　次の各変換は、static_cast、const_cast、reinterpret_cast、Ｃ言語形式のキャスト、キャストは不要、のいずれで行えるか答えてください。
+
+	// long → short
+	// bool → int
+	// int* → int
+	// const char* → char*
+	// struct MyData* → void*
+	// struct MyData* → const struct MyData*
+	// void* → struct MyData*
+	// const float* → int*
+	// bool (*f)(int) → bool (*f)(const char*)
+}
